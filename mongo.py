85dfd99756de5fa352be664f16fb4df1ac7bfa7a
nunocosta <91nunocosta@gmail.com>
2018-11-11 21:23:26 +0000

    extracts ConfiguredMongoMixin

---
+++ mongo.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+
+class MissingConfigurationParameter(Exception):
+
+    def __init__(self, parameter):
+        super().__init__('{} parameter is missing in the configuration.'.format(parameter))
+
+class ConfiguredMongoMixin:
+
+    REQUIRED_PARAMETERS = ['MONGO_HOST', 'MONGO_PORT', 'MONGO_DB']
+
+    def configure_mongo(self, config):
+        self.db_config = config
+
+    def open_db(self):
+        self.__validate_db_config()
+        client = MongoClient(
+            host=self.db_config['MONGO_HOST'], port=self.db_config['MONGO_PORT'])
+        self.database = client[self.db_config['MONGO_DB']]
+
+    def __validate_db_config(self):
+        for parameter in ConfiguredMongoMixin.REQUIRED_PARAMETERS:
+            if parameter not in self.db_config:
+                raise MissingConfigurationParameter(parameter)
---
+++ tests/test_mongo.py
@@ -0,0 +1,56 @@
+from unittest import TestCase
+
+import mongomock
+
+from scrapter import mongo
+
+class TestConfiguredMongoMixin(TestCase):
+
+    @mongomock.patch(servers=(('mongo', 27017),))
+    def test_can_open_db(self):
+        import scrapter
+        scrapter.mongo.MongoClient = mongomock.MongoClient
+        db_config = {
+            'MONGO_HOST': 'mongo',
+            'MONGO_PORT': 27017,
+            'MONGO_DB': 'db'
+        }
+        register = scrapter.mongo.ConfiguredMongoMixin()
+        register.configure_mongo(db_config)
+        register.open_db()
+        self.assertIsNotNone(register.database)
+        self.assertEqual(register.database.name, 'db')
+        self.assertEqual(register.database.client.host, 'mongo')
+        self.assertEqual(register.database.client.port, 27017)
+
+    @mongomock.patch(servers=(('mongo', 27017),))
+    def test_cant_open_db_with_missing_configs(self):
+        import scrapter
+        scrapter.mongo.MongoClient = mongomock.MongoClient
+        db_config = {
+            'MONGO_HOST': 'mongo',
+            'MONGO_PORT': 27017,
+        }
+        register = scrapter.mongo.ConfiguredMongoMixin()
+        register.configure_mongo(db_config)
+        with self.assertRaises(scrapter.mongo.MissingConfigurationParameter) as cm:
+            register.open_db()
+        self.assertTrue('MONGO_DB' in str(cm.exception))
+        db_config = {
+            'MONGO_HOST': 'mongo',
+            'MONGO_DB': 'db',
+        }
+        register = scrapter.mongo.ConfiguredMongoMixin()
+        register.configure_mongo(db_config)
+        with self.assertRaises(scrapter.mongo.MissingConfigurationParameter) as cm:
+            register.open_db()
+        self.assertTrue('MONGO_PORT' in str(cm.exception))
+        db_config = {
+            'MONGO_PORT': 27017,
+            'MONGO_DB': 'db'
+        }
+        register = scrapter.mongo.ConfiguredMongoMixin()
+        register.configure_mongo(db_config)
+        with self.assertRaises(scrapter.mongo.MissingConfigurationParameter) as cm:
+            register.open_db()
+        self.assertTrue('MONGO_HOST' in str(cm.exception))
