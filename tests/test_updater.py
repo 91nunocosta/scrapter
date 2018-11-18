7811afbabece6be9fd783e0c278ad04b45a7b159
nunocosta <91nunocosta@gmail.com>
2018-11-18 01:42:29 +0000

    Updater can register successful update

--- tests/test_updater.py
+++ tests/test_updater.py
@@ -1,15 +1,19 @@
+from codecs import register
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
 
 from scrapy.settings import Settings
+from unittest.mock import MagicMock, patch
 
 import scrapter.updater
+
 
 class TestUpdater(TestCase):
 
     def setUp(self):
         self.crawl_patcher = patch('scrapter.updater.CrawlerProcess')
+        self.register_patcher = patch('scrapter.updater.MongoUpdatesRegister')
         self.crawl_mock = self.crawl_patcher.start()
+        self.register_mock = self.register_patcher.start()
 
         spiders = ['spider1', 'spider2']
         settings = Settings({
@@ -19,8 +23,8 @@ class TestUpdater(TestCase):
         self.assertListEqual(self.updater.spiders, spiders)
 
     def tearDown(self):
-            # self.crawl_patcher.stop()
-        pass
+        self.crawl_patcher.stop()
+        self.register_patcher.stop()
 
     def test_can_start(self):
         self.updater.start()
@@ -28,3 +32,10 @@ class TestUpdater(TestCase):
         crawl_process.crawl.assert_any_call('spider1')
         crawl_process.crawl.assert_any_call('spider2')
         crawl_process.start.assert_called()
+
+    def test_can_register_successful_update(self):
+        register = self.register_mock.return_value
+        register.start = MagicMock(return_value='1')
+        self.updater.start()
+        register.start.assert_called_with(['spider1', 'spider2'])
+        register.succeed.assert_called_with('1')
--- updater.py
+++ updater.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 from scrapy.crawler import CrawlerProcess
 
+from scrapter.mongo_updates_register import MongoUpdatesRegister
+
 class UpdateStatus(Enum):
     CREATED = 'created'
     STARTED = 'started'
@@ -13,9 +15,12 @@ class Updater:
     def __init__(self, settings):
         self.settings = settings
         self.spiders = settings.get('SPIDERS')
+        self.register = MongoUpdatesRegister(settings)
 
     def start(self):
         process = CrawlerProcess(self.settings)
         for spider in self.spiders:
             process.crawl(spider)
+        update_id = self.register.start(self.spiders)
         process.start()
+        self.register.succeed(update_id)
