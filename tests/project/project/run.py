30b1d44a1302776641ebc7b9fbbb110999b6c688
nunocosta-daltix <nuno.costa@daltix.com>
2019-02-13 15:33:51 +0000

    change project run script

---
+++ tests/project/project/run.py
@@ -0,0 +1,4 @@
+from scrapter.run import execute
+
+if __name__ == "__main__":
+    execute()
--- tests/project/run.py
+++
@@ -1,6 +0,0 @@
-from scrapy.utils.project import get_project_settings
-from scrapter.updater import Updater
-
-
-updater = Updater(get_project_settings())
-updater.run()
